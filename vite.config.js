d361fa30106ac561b39824124646e05e358ffa0d
Vaibhav-Kaul <shivamkaul11@gmail.com>
2024-12-07 20:19:10 +0530

    fix: threejs version issue

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,20 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'SVK04.github.io',
+  base: '/SVK04.github.io/',
   build: {
     chunkSizeWarningLimit: 100000000,
+    rollupOptions: {
+      input: 'src/main.jsx',
+    },
+    
   },
   css: {
-    postcss: './postcss.config.json', 
+    postcss: './postcss.config.json',
   },
   server: {
-    open: true, 
-    port: process.env.PORT, 
+    open: true,
+    port: process.env.PORT,
   },
 });
 
