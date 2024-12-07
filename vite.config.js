f1491067fb9e08c17855ac30f484474d8ebc401c
Vaibhav-Kaul <shivamkaul11@gmail.com>
2024-12-07 19:18:59 +0530

    fix: github workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'SVK04.github.io',
   build: {
     chunkSizeWarningLimit: 100000000,
   },
